fdbe1e61a595dab72434c571958ccda80f90fd73
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-07 11:49:22 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- *Description: main - Prints out a string
+ *Description: main - entry point
  * Return :Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
