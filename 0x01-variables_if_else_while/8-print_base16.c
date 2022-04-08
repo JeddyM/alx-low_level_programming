31caf9a8df2df4446dbe9d4476fb1b99ec5c1fe0
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-08 13:23:03 -0700

    Task 8 and 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point prints lowercase
+ * base 16 numbers
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+
+	char ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		putchar(ch);
+	}
+	for(ch = 97; ch <= 102; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point print possible 
+ * combination of single digit
+ * Return: always 0 (success)
+ */
+int main(void)
+
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+	
+	putchar(num);
+
+	if (num == 57)
+	{
+	break;
+	}
+
+	putchar(',');
+	putchar(' ');
+
+
+	}
+
+	putchar('\n');
+	return (0);
+
+}
