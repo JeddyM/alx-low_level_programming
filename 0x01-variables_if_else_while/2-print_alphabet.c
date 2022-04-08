7601cf44b4d22b23664d4adcf011b6ed744c0643
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-08 07:07:23 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet letters in lowercase
+ * and prints a new line
+ * Return : always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 25; ++i)
+	putchar('a' + i);
+
+	putchar("\n");
+	return (0)
+}	
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+*main -prints out in lowercase and
+* then in uppercase
+*Return:Always 0(success)
+*/
+int main(void)
+
