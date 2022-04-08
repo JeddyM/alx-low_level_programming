69d0f118352caacb9e358df6a5a34c2d4ca187e2
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-08 12:33:50 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point prints lowercase in reverse
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+	char ch;
+
+	for(ch = 'z'; ch >= 'a'; ch--)
+	{
+	putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+
+}
