7c527cfba055f1a542889802f8af0ca879ff5b2c
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-11 03:03:50 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 
-	int a,
-	int b,
-	int c,
+	int a;
+	int b;
+	int c;
 
-	for (a = 48; n < 58; a++)
+	for (a = 48; a < 58; a++)
 	{
 		for(b = 49; b < 58; b++)
 		{
@@ -25,7 +25,7 @@ int main(void)
 					putchar(a);
 					putchar(b);
 					putchar(c);
-					if (a != 50 || m != 56)
+					if (a != 50 || b != 56)
 					{
 					putchar(',');
 					putchar(' ');
