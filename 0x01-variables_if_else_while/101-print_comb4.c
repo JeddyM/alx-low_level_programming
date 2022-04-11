821f8e0152246e8363d5311d4317448cb446b181
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-11 02:33:46 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point prints 3 digit numbers
+ * ascending order
+ * Return : always 0 (success)
+ */
+
+int main(void)
+{
+
+	int a,
+	int b,
+	int c,
+
+	for (a = 48; n < 58; a++)
+	{
+		for(b = 49; b < 58; b++)
+		{
+
+			for (c = 50; c < 58; c++)
+			{
+
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 50 || m != 56)
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
+
+	}
+
+	putchar('\n');
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
