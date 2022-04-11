0686ce6bd4d4060d0250da96e392e91d606c4331
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-11 04:54:53 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@ int main(void)
 {
 
 	int a, b;
-	for (a = 0; a <= 98 a++)
+	for (a = 0; a <= 98; a++)
 	{
 
 		for (b = a+1; b <= 99; b++)
@@ -23,8 +23,8 @@ int main(void)
 			if (a ==98 && b == 99)
 			       continue;
 			putchar (',');
-	 		putchar(' ');	
-	
+	 		putchar(' ');
+
 		}
 	}
 
