275013c0c2b5ee4133e0d1ffa892318fe8961424
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-12 05:10:25 -0700

    Task 3 and 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - checks where c is alphabetic character
+ *
+ * Return: 1 if alphabetic character else 0
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 'a' && c =< 'z') || (c >= 'A' && c =< 'Z'))
+	{
+		return (1);
+
+	}
+	else
+	{
+		return (0);
+
+	}
+}
