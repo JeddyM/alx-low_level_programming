baa06a1e44641250d6c5d269b86b7721929cda66
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-12 08:59:08 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,87 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: always 0 (success)
+ */
+
+void times_table(void)
+
+{
+
+	int i;
+
+	int j;
+
+	int c;
+
+	for (i = 0; i <= 9; i++)
+
+	{
+
+		for (j = 0; j <= 9; j++)
+
+		{
+
+			c = i * j;
+
+			if ((c / 10) == 0)
+
+			{
+
+				if (j == 0)
+
+				{
+
+					_putchar ('0');
+
+				}
+
+				if (j != 0)
+
+				{
+
+					_putchar (' ');
+
+					_putchar ((c % 10) + '0');
+
+				}
+
+				if (j < 9)
+
+				{
+
+					_putchar(',');
+
+					_putchar (' ');
+
+				}
+
+			}
+
+			else
+
+			{
+
+				_putchar ((c / 10) + '0');
+
+				_putchar ((c % 10) + '0');
+
+				if (j < 9)
+
+				{
+					_putchar(',');
+
+					_putchar (' ');
+
+				}
+
+			}
+
+		}
+
+		_putchar ('\n');
+
+	}
+
+}
