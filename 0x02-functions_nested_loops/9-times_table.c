ad65af1382a49bc83d9ed8844a391452f926b343
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-12 09:12:45 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,46 +13,34 @@ void times_table(void)
 	int c;
 
 	for (i = 0; i <= 9; i++)
-
 	{
 		for (j = 0; j <= 9; j++)
-
 		{
 			c = i * j;
 
 			if ((c / 10) == 0)
-
 			{
 				if (j == 0)
-
 				{
 					_putchar ('0');
 				}
-
 				if (j != 0)
-
 				{
 					_putchar (' ');
 					_putchar ((c % 10) + '0');
 				}
-
 				if (j < 9)
-
 				{
 				_putchar(',');
 				_putchar (' ');
 				}
 			}
-
 			else
-
 			{
-
 				_putchar ((c / 10) + '0');
 				_putchar ((c % 10) + '0');
 
 				if (j < 9)
-
 				{
 				_putchar(',');
 				_putchar (' ');
@@ -61,5 +49,4 @@ void times_table(void)
 		}
 		_putchar ('\n');
 	}
-
 }
