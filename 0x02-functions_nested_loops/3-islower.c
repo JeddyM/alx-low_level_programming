0d0e179281af6e48546cbe785f0f29958ee91b1a
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-12 06:31:30 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - checks if a character is lowercase
- *
+ *@c: The chacter to be checked
  * Return: 1 if a character is lowercase 0 otherwise
  */
 int _islower(int c)
@@ -13,5 +13,5 @@ int _islower(int c)
 	return (1);
 	else
 	return (0);
-	
+
 }
