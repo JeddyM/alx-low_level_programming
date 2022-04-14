751d8653707a380969da5ce4294154ba32b21dfb
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-14 15:26:26 -0700

    Task 8 and 9

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle
+ * @size:size of the triangle
+ *
+ * Return : always 0 (success)
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+
+	{
+	_putchar('\n');
+	}
+
+	else
+	{
+	
+		int a, b;
+
+		for (i = 1; i <= size; i++)
+		{
+
+			for (j = i; j < size; j++)
+			{
+
+			_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+
+			_putchar('#');
+			}
+
+			_putchar('\n');
+
+		}
+
+	}
+}
