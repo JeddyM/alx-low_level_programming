32ee2ff44de451ec722d9a498f303926102307d3
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-14 14:24:34 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print-diagonal - draws a diagonal line
+ * @n: number of times the character should be printed
+ * Return: always 0 (success)
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+
+	else
+	{
+
+	int a, b;
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < n; b++)
+		{
+
+			if (a == b)
+				_putchar('\\');
+
+			else if (b < a)
+				putchar(' ');
+		}
+
+		_putchar('\n');
+
+	}
+	}
+}
