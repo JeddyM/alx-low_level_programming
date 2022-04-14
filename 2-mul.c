3d72739245c591180ed4a3a6b16cdafcedb7c486
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-14 03:50:36 -0700

    Task 2

---
+++ 2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: input integer
+ * @b: input integer
+ * Return: always 0 (success)
+ */
+int mul(int a, int b)
+{
+return(a * b);
+}
