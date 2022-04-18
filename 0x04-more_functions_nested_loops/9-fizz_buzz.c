4fa583bb5435e8750b4f6f4f9725b87a3e413fda
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-18 10:56:20 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,10 +19,10 @@ int main(void)
 			printf(" Buzz");
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf(" FizzBuzz");
-		else if(i == 1)
-		printf("%d", i);
-		else
+		else if (i == 1)
 			printf("%d", i);
+		else
+			printf(" %d", i);
 	}
 	printf("\n");
 	return (0);
