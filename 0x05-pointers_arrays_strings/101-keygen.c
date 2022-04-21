201bc974a18b6a38fe69baa83f74e25240277bfa
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-21 07:50:41 -0700

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+/**
+ * main - print  random valid passwords for the program 101-crackme
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+
+
+
+
+}
+ 
