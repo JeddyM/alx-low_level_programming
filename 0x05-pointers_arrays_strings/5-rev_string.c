06b354d7f7520ff03a97da015b2de9ad3c431ac7
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-21 03:51:50 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - recerses a  string
+ * @s: string pointer
+ * Return void
+ */
+void rev_string(char *s)
+{
+int length, j, i;
+char a, b;
+
+for (length = 0; s[length] != '\0'; length++)
+{
+}
+	j = lenght - 1;
+	i = 0;
+
+	while( j > i)
+	{
+
+	a = s[i];
+	b = s[j];
+	s[i] = b;
+	s[j] = a;
+	j--;
+	i++;
+	}
+}
