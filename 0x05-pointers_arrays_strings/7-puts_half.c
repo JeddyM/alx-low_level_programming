ba3f3bd3af777019f9a924e9062e703f6cb87e6c
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-21 05:39:30 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string followed by new linw
+ * @str: pointer string
+ * Return: always 0 success
+ */
+void puts_half(char *str)
+{
+	int length, h, i;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+	}
+
+	h = (lenght - 1) / 2
+	
+	for (i = h + 1; str[i] != '\0'; i++)
+	{
+	
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}
