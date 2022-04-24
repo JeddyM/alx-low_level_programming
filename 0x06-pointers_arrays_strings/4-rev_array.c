bad976128354316e9afbf510f7311a8e4c4e6640
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-24 13:12:24 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,17 +6,14 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, j, tmp
+	int i, j;
 
-	i = 0;
 	j = n - 1;
-	while (i < j)
-
+	for (i = 0; i < n / 2; i++)
 	{
 	tmp = a[i];
 	a[i] = a[j];
 	a[j] = tmp;
-	i++;
 	j--;
 	}
 }
