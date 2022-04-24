55ab15eef96cda271ddde61fad5b1f22c27df0ce
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-24 05:34:38 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * *_strncpy - copies a string
+ * @dest: string pointer
+ * @src: string pointer
+ * @n: number of bytes to use from src
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int i = 0;
+	int j = 0;
+
+	while (n > j)
+	{
+		if (src[j] == '\0')
+			for (k < n; k++)
+			{
+			dest[i] = '\0';
+			i++;
+			}
+	}
+	else
+	{
+	dest[i] = src[j];
+	j++;
+	i++;
+	}
+
+	return (dest);
+}
