7a6faebfa8fb1d693e9bce727da78a9681de7112
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-25 02:44:21 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,20 +4,24 @@
  * @dest: string pointer
  * @src: string pointer
  * @n: number of bytes from src
- * Return: :wq
- * a pointer to the resulting string dest
+ * Return: a pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	for (j = 0; src[j] != '\0' && n > j; i++, j++)
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && n > j)
+	{
 		dest[i] = src[j];
+		j++;
+		i++;
 
+	}
 	if (n > 0)
 	{
-	dest[i] = '\0';
+		dest[i] = '\0';
 	}
 	return (dest);
 }
