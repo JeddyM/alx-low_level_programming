5cbd0303a1e6b56814e22c7313511283193f2bac
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-25 00:49:57 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
-	for (j = 0; src[j] != '\0' && n > j, i++, j++)
+	for (j = 0; src[j] != '\0' && n > j; i++, j++)
 		dest[i] = src[j];
 
 	if (n > 0)
