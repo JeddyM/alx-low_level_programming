16e2f929153edb24e0aa2e07120666d8dd0d93dd
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-28 11:07:57 -0700

    Task 4

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * result- evaluates from 1 to n
+ * @a: equal to n
+ * @i: iterates from 1 to n
+ * Return: square root else -1 if none
+ */
+
+int result(int a, i)
+{
+	int square;
+
+	square = i *i;
+	if (square == a)
+		return (i);
+	else if (square < a)
+		return (result(a, i + 1));
+	return (-1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ * Return: square root else -1 if none
+ */
+int _sqrt_recursion(int n)
+{
+
+return (result(n, 1));
+}
