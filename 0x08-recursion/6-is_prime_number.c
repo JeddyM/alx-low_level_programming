574769cf81d40c44ac8ba9998a217136fb0ef303
Jeddy Mweru <jeddymweru@gmail.com>
2022-04-29 08:31:23 -0700

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,14 +4,24 @@
  * @n: number
  * Return: 1 if prime number,otherwise return 0
  */
-int is_prime_number(int n);
+int is_prime_number(int n)
 {
-	if (*s == '\0')
-		_putchar('\n');
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(++s);
-	}
 
+	if (n < 2)
+		return (0);
+	return (prime_find(n, 2));
+}
+/**
+ * prime_find - function to find prime number
+ * @a: same as number to evaluate
+ * @i: number to iterate from 1 to n
+ * Return: 1 success
+ */
+int prime_find(int a, int i)
+{
+	if (a == i)
+		return (1);
+	else if (a % i == 0)
+		return (0);
+	return (prime_find(a, i + 1));
 }
