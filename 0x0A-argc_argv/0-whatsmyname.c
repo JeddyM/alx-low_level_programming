7fe42f95ba3a28dcaa471163eef29c3e24f07241
Jeddy Mweru <jeddymweru@gmail.com>
2022-05-08 10:54:51 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - main function
+ * main - prints its name
  * @argc: number of arguments
  * @argv: pointer of array of pointer containing argument strings
  * Return: Always 0 success
  */
 
-int main(int argc, char **argv)
+int main(int argc , char **argv)
+
 {
+	if (argc > 0)
 	printf("%s\n", argv[0]);
 
 	return (0);
