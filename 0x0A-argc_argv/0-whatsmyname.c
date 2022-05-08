946565223feaa289e4603e12e4f610b20d355e7b
Jeddy Mweru <jeddymweru@gmail.com>
2022-05-08 08:23:22 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,11 +7,9 @@
  * Return: Always 0 success
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-
-	for (argc > 0)
-		printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
