8654e5364e62d5d4e528e78f2fb2952965798276
Jeddy Mweru <jeddymweru@gmail.com>
2022-05-16 12:32:29 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * print_name - function that prints name
+ * @name: name as a parameter passed
+ * @f: function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+	if ((*f) != NULL)
+	(*f)(name);
+
+
+}
+
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - function that executes a function given as a parameter on each element of an array
+ * @size: Size of the array
+ * @action: function pointer
+ * @array: pointer to an array declared in main fuction
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+if ((*f) != NUll)
+	(*f)(name);
+
+
+}
+
