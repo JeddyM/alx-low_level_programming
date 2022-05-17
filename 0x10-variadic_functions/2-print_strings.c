22b0f6a617724377a6b19d98355b37e296dcc645
Jeddy Mweru <jeddymweru@gmail.com>
2022-05-17 16:09:17 -0700

    Print string in variadic function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,7 +29,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (a)
 			printf("%s", a);
 		if (a == NULL)
-			printf("nil)");
+			printf("(nil)");
 
 		if (separator != NULL && i != n - 1)
 		{
