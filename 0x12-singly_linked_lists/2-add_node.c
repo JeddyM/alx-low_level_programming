6b67d665a768b267729f941c32dc1b783f47831c
Jeddy Mweru <jeddymweru@gmail.com>
2022-05-25 02:25:55 -0700

    Including the string library

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * add_node- adds a new node at the beginning of a list_t list.
  * @head: pointer to head of list-t list
