10ef5fc2e8d738f0b4482250ad817fe94d860fce
Jeddy Mweru <jeddymweru@gmail.com>
2022-05-25 01:15:08 -0700

    Task 0

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,11 +9,13 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t count;
+	size_t count = 0;
 	/*count of elements*/
 
-	for (count = 0; h != NULL; count++)
+	while (h != NULL)
+
 	{
+		count++;
 		h = h->next;
 	}
 	return (count);
