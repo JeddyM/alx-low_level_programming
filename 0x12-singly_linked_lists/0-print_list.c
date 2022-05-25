93895ba9e7a89c30b300ff2793e362e9af001f08
Jeddy Mweru <jeddymweru@gmail.com>
2022-05-24 23:56:49 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,13 @@ size_t print_list(const list_t *h)
 {
 	size_t count;
 
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
 
 	for (count = 0; h != NULL; count++)
 	{
-		printf("[%i] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
 		h = h->next;
 	}
 	return (count);
