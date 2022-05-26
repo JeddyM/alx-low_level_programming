a66704bedc770614d86f3adaca70ea6cabf19b18
Jeddy Mweru <jeddymweru@gmail.com>
2022-05-26 18:17:46 +0000

    Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,20 +21,22 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
-	current_node = *head;
-	while (*head == NULL)
+
+	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 		/*nothing on list no current_node*/
 	}
 
-
-	if (current_node)
+	else
+	{
+		current_node = *head;
+		while (current_node->next != NULL)
+			current_node->next = current_node;
 
 		current_node->next = new_node;
-	else
-		*head = new_node;
+	}
 
 	return (new_node);
 
