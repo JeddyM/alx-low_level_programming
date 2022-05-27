bc77b26df1a399c96bb7e002955a46daaeaaf0e1
Jeddy Mweru <jeddymweru@gmail.com>
2022-05-27 14:44:53 +0000

    Insert node task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (i = 0; i < (idx - 1); i++)
 	{
 
-		if (current_node == NULL)
+		if (current_node->next == NULL)
 			return (NULL);
 
 		current_node = current_node->next;
