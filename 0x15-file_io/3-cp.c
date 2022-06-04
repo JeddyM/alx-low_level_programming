4f26d9f88e7e25c24747aaa443fb22e6ba5f1353
Jeddy Mweru <jeddymweru@gmail.com>
2022-06-04 19:50:03 +0000

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -31,36 +31,40 @@
 
 int main(int ac, char *av[])
 {
-	int fd_source, fd_dest, wr, rd;
+	int fd_source, fd_dest, wr, rd, cl;
 	char buf[1204];
+	char *file_from, *file_to;
 
 	if (ac != 3)
 		dprintf(SDE, "Usage: cp file_from file_to\n"), exit(97);
+
+	file_from = av[1];
+	file_to = av[2];
 	fd_source = open(av[1], O_RDONLY);
 	if (fd_source == -1)
-		dprintf(SDE, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(SDE, "Error: Can't read from file %s\n", file_from), exit(98);
 	fd_dest = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_dest == -1)
-		dprintf(SDE, "Error: Can't write to %s\n", av[2]), exit(99);
+		dprintf(SDE, "Error: Can't write to %s\n", file_to), exit(99);
 	while (rd > 0)
 	{
 		rd = read(fd_source, buf, 1204);
 		if (rd == -1)
 		{
-			dprintf(SDE, "Error: Can't read from file %s\n", av[1]), exit(98);
+			dprintf(SDE, "Error: Can't read from file %s\n", file_from), exit(98);
 		}
 		if (rd > 0)
 		{
 			wr = write(fd_dest, buf, (ssize_t) rd);
 			if (wr == -1)
-				dprintf(SDE, "Error: Can't write to %s\n", av[2]), exit(99);
+				dprintf(SDE, "Error: Can't write to %s\n", file_to), exit(99);
 		}
 	}
-	rd = close(fd_source);
-	if (rd == -1)
+	cl = close(fd_source);
+	if (cl == -1)
 		dprintf(SDE, "Error: Can't close fd %d\n", fd_source), exit(100);
-	wr = close(fd_dest);
-	if (wr == -1)
+	cl = close(fd_dest);
+	if (cl == -1)
 		dprintf(SDE, "Error: Can't close fd %d\n", fd_dest), exit(100);
 	return (0);
 }
