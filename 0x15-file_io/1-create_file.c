c9b777ffeabe0bd42b3cf4005a01b8012d0036b5
Jeddy Mweru <jeddymweru@gmail.com>
2022-06-16 11:21:14 +0000

    Fixing file errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,9 +23,6 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, len, wr;
 
-	/* opening and creating*/
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
 	if (filename == NULL)
 		return (-1);
 
@@ -35,6 +32,8 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
+	 /* opening and creating*/
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	/*writing*/
 
 	wr = write(fd, text_content, len);
